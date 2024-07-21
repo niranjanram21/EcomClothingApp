af0769e4dab5b4c06a2adbc756ebe5478a5b896d
niranjanram21 <niranjanram3657@gmail.com>
2024-07-21 12:33:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const multer = require("multer");
 const path = require("path");
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors());
